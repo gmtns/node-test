a37d6877b2f7c7eee60eb268a8782e0d9bdf7fd2
GABRIEL MARTINS <gabrielmms31@gmail.com>
2020-07-12 21:21:53 -0300

    refactor: remove unused stubs/dependencies from FetchImagesByUrl  test

--- src/data/url-management/fetch-images-by-url.spec.ts
+++ src/data/url-management/fetch-images-by-url.spec.ts
@@ -1,7 +1,6 @@
-import { IDownloadImagesFromSrcRepository } from 'data/protocols/dowloand-image-from-src'
-import { IFetchImagesSrcFromUrlRepository } from 'data/protocols/fetch-image-src-from-url'
 import { IFindByUrlRepository } from 'data/protocols/find-by-url-repository'
-import { ISaveOrReplaceWebsiteImageUrl } from 'data/protocols/save-or-replace-image-src'
+import { IWebHandler } from 'data/protocols/libs/web-hander/web-handler'
+import { ISaveOrReplaceWebsiteImageUrlRepository } from 'data/protocols/save-or-replace-image-src'
 import { WebsiteImageModel } from 'domain/models/website-image-model'
 import faker from 'faker'
 import { FetchImagesByUrl } from './fetch-images-by-url'
@@ -13,11 +12,11 @@ const fakeWebsiteModel: WebsiteImageModel = {
   webImagesPath: [faker.internet.url()]
 }
 
-class GetAllImageSrcRepositorySpy implements IFetchImagesSrcFromUrlRepository {
-  async getAllImagesSrc (websiteUrl): Promise<string[]> {
-    return ['any_url_string']
-  }
-}
+// class GetAllImageSrcRepositorySpy implements IFetchImagesSrcFromUrlRepository {
+//   async getAllImagesSrc (websiteUrl): Promise<string[]> {
+//     return ['any_url_string']
+//   }
+// }
 
 class FindByUrlRepositorySpy implements IFindByUrlRepository {
   async findByUrl (url: string): Promise<WebsiteImageModel[]> {
@@ -25,38 +24,41 @@ class FindByUrlRepositorySpy implements IFindByUrlRepository {
   }
 }
 
-class SaveOrReplaceWebsiteImageUrlSpy implements ISaveOrReplaceWebsiteImageUrl {
+class SaveOrReplaceWebsiteImageUrlSpy implements ISaveOrReplaceWebsiteImageUrlRepository {
   async saveOrReplace (url: string, imagesSrc: string[]): Promise<WebsiteImageModel[]> {
     return [fakeWebsiteModel]
   }
 }
 
-class DownloadImagesSpy implements IDownloadImagesFromSrcRepository {
-  async downloadImages (imagesSrc: string[]): Promise<string[]> {
-    return ['any_url']
+// class DownloadImagesSpy implements IDownloadImagesFromSrcRepository {
+//   async downloadImages (imagesSrc: string[]): Promise<string[]> {
+//     return ['any_url']
+//   }
+// }
+
+export class WebHandlerSpy implements IWebHandler {
+  async downloadImagemFrom (url: string): Promise<string[]> {
+    return ['path/image.png']
   }
 }
 
 type SutType = {
   sut: FetchImagesByUrl
-  getAllImageSrcSpy: GetAllImageSrcRepositorySpy
   findByUrlSpy: FindByUrlRepositorySpy
   saveOrReplace: SaveOrReplaceWebsiteImageUrlSpy
-  downloadImagesSpy: DownloadImagesSpy
+  webHandlerSpy: IWebHandler
 }
 
 const sutFactory = (): SutType => {
   const findByUrlSpy = new FindByUrlRepositorySpy()
-  const getAllImageSrcSpy = new GetAllImageSrcRepositorySpy()
   const saveOrReplace = new SaveOrReplaceWebsiteImageUrlSpy()
-  const downloadImagesSpy = new DownloadImagesSpy()
-  const sut = new FetchImagesByUrl(findByUrlSpy, getAllImageSrcSpy, saveOrReplace, downloadImagesSpy)
+  const webHandlerSpy = new WebHandlerSpy()
+  const sut = new FetchImagesByUrl(findByUrlSpy, saveOrReplace, webHandlerSpy)
   return {
     sut,
-    getAllImageSrcSpy,
     findByUrlSpy,
     saveOrReplace,
-    downloadImagesSpy
+    webHandlerSpy
   }
 }
 
@@ -85,57 +87,34 @@ describe('FetchImagesByUrl', () => {
     await expect(result).rejects.toThrow()
   })
 
-  test('should call getAllImagesSrc with correct params if  override is true', async () => {
-    const { sut, getAllImageSrcSpy } = sutFactory()
-    const spyOn = jest.spyOn(getAllImageSrcSpy, 'getAllImagesSrc')
+  test('should call downloadImageFrom with correct params', async () => {
+    const { sut, webHandlerSpy } = sutFactory()
+    const spyOn = jest.spyOn(webHandlerSpy, 'downloadImagemFrom')
     await sut.extractImage(fakeUrl, true)
     expect(spyOn).toHaveBeenCalledWith(fakeUrl)
   })
 
-  test('should throws if getAllImages throws', async () => {
-    const { sut, getAllImageSrcSpy } = sutFactory()
-    jest.spyOn(getAllImageSrcSpy, 'getAllImagesSrc').mockImplementationOnce(() => {
+  test('should throw if downloadImage from throws', async () => {
+    const { sut, webHandlerSpy } = sutFactory()
+    jest.spyOn(webHandlerSpy, 'downloadImagemFrom').mockImplementationOnce(() => {
       throw new Error()
     })
     const result = sut.extractImage(fakeUrl, true)
     await expect(result).rejects.toThrow()
   })
 
-  test('should return null if getImages not  found images', async () => {
-    const { sut, getAllImageSrcSpy } = sutFactory()
-    jest.spyOn(getAllImageSrcSpy, 'getAllImagesSrc').mockReturnValueOnce(new Promise(resolve => resolve([])))
+  test('should return null if downloadImageFrom no has paths', async () => {
+    const { sut, webHandlerSpy } = sutFactory()
+    jest.spyOn(webHandlerSpy, 'downloadImagemFrom').mockReturnValueOnce(new Promise(resolve => resolve([])))
     const result = await sut.extractImage(fakeUrl, true)
     expect(result).toBeNull()
-  })
-
-  test('should call downloadImages if getAllImages return a valid array', async () => {
-    const { sut, downloadImagesSpy } = sutFactory()
-    const spyOn = jest.spyOn(downloadImagesSpy, 'downloadImages')
-    await sut.extractImage(fakeUrl, true)
-    expect(spyOn).toHaveBeenCalledWith(['any_url_string'])
-  })
-
-  test('should throws if downloadImagesSpy throws', async () => {
-    const { sut, downloadImagesSpy } = sutFactory()
-    jest.spyOn(downloadImagesSpy, 'downloadImages').mockImplementationOnce(() => {
-      throw new Error()
-    })
-    const result = sut.extractImage(fakeUrl, true)
-    await expect(result).rejects.toThrow()
-  })
-
-  test('should return null if localPaths return an empty array', async () => {
-    const { sut, downloadImagesSpy } = sutFactory()
-    jest.spyOn(downloadImagesSpy, 'downloadImages').mockReturnValueOnce(new Promise(resolve => resolve([])))
-    const result = await sut.extractImage(fakeUrl, true)
-    await expect(result).toBeNull()
   })
 
   test('should call saveOrReplace with correct params', async () => {
     const { sut, saveOrReplace } = sutFactory()
     const spyOn = jest.spyOn(saveOrReplace, 'saveOrReplace')
     await sut.extractImage(fakeUrl, true)
-    expect(spyOn).toHaveBeenCalledWith(fakeUrl, ['any_url'])
+    expect(spyOn).toHaveBeenCalledWith(fakeUrl, ['path/image.png'])
   })
 
   test('should throws if saveOrReplace throws', async () => {
@@ -153,3 +132,49 @@ describe('FetchImagesByUrl', () => {
     expect(result).toEqual([fakeWebsiteModel])
   })
 })
+
+// test('should call getAllImagesSrc with correct params if  override is true', async () => {
+//   const { sut, getAllImageSrcSpy } = sutFactory()
+//   const spyOn = jest.spyOn(getAllImageSrcSpy, 'getAllImagesSrc')
+//   await sut.extractImage(fakeUrl, true)
+//   expect(spyOn).toHaveBeenCalledWith(fakeUrl)
+// })
+
+// test('should throws if getAllImages throws', async () => {
+//   const { sut, getAllImageSrcSpy } = sutFactory()
+//   jest.spyOn(getAllImageSrcSpy, 'getAllImagesSrc').mockImplementationOnce(() => {
+//     throw new Error()
+//   })
+//   const result = sut.extractImage(fakeUrl, true)
+//   await expect(result).rejects.toThrow()
+// })
+
+// test('should return null if getImages not  found images', async () => {
+//   const { sut, getAllImageSrcSpy } = sutFactory()
+//   jest.spyOn(getAllImageSrcSpy, 'getAllImagesSrc').mockReturnValueOnce(new Promise(resolve => resolve([])))
+//   const result = await sut.extractImage(fakeUrl, true)
+//   expect(result).toBeNull()
+// })
+
+// test('should call downloadImages if getAllImages return a valid array', async () => {
+//   const { sut, downloadImagesSpy } = sutFactory()
+//   const spyOn = jest.spyOn(downloadImagesSpy, 'downloadImages')
+//   await sut.extractImage(fakeUrl, true)
+//   expect(spyOn).toHaveBeenCalledWith(['any_url_string'])
+// })
+
+// test('should throws if downloadImagesSpy throws', async () => {
+//   const { sut, downloadImagesSpy } = sutFactory()
+//   jest.spyOn(downloadImagesSpy, 'downloadImages').mockImplementationOnce(() => {
+//     throw new Error()
+//   })
+//   const result = sut.extractImage(fakeUrl, true)
+//   await expect(result).rejects.toThrow()
+// })
+
+// test('should return null if localPaths return an empty array', async () => {
+//   const { sut, downloadImagesSpy } = sutFactory()
+//   jest.spyOn(downloadImagesSpy, 'downloadImages').mockReturnValueOnce(new Promise(resolve => resolve([])))
+//   const result = await sut.extractImage(fakeUrl, true)
+//   await expect(result).toBeNull()
+// })
